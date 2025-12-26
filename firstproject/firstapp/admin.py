01c875fffa7f02ea3decce5821d5a911285438ce
Pushkar Kathayat <pushkarsingh32@gmail.com>
2025-12-26 08:45:01 +0530

    Delete firstapp directory and all its files
    - Removed all firstapp files from repository
    - Prevents import errors from deleted app

--- firstproject/firstapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Reservervation
-# Register your models here.
-
-admin.site.register(Reservervation)
--- firstproject/firstapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-
-
-urlpatterns = [
-    path('function', views.hello_world),
-    path('class', views.HelloEthiopia.as_view()),
-    path('reservation', views.home)
-]
